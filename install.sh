dde3fc57496369a211a0bec52a2bf7b72aca84a6
eric <ericjesus403@gmail.com>
2026-09-19 22:29:27 -0300

    fix: download docker compose docker

--- install.sh
+++ install.sh
@@ -59,6 +59,8 @@ Architectures: $(dpkg --print-architecture)
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 apt update
 groupadd docker
 usermod -aG docker $USER
