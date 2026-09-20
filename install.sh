bdc1263458c11799262c3649061e9d0e3dd8c2c8
eric <ericjesus403@gmail.com>
2026-09-19 22:00:35 -0300

    feat: add step download and config docker

--- install.sh
+++ install.sh
@@ -44,3 +44,25 @@ fi
 rm -f "$SUDOERS_TEMP"
 
 
+apt update
+apt install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+apt update
+groupadd docker
+usermod -aG docker $USER
+newgrp docker
+
+
+
